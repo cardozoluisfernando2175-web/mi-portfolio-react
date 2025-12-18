02eb937a697cfcf63053584a4201c925d9e4d4ef
Luis Fernando Cardozo Vargas <cardozoluisfernando2175@gmail.com>
2025-12-18 02:10:14 -0400

    fix contacto img css emailjs

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -1,55 +1,33 @@
-import { useEffect, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import "../styles/contacto.css";
 import dibujo from "../assets/dibujo_web.svg";
 
 export default function Contacto() {
 
-    const textareaRef = useRef(null);
-
     const enviarCorreo = (e) => {
         e.preventDefault();
 
         emailjs.sendForm(
-            import.meta.env.VITE_EMAILJS_SERVICE,
-            import.meta.env.VITE_EMAILJS_TEMPLATE,
+            "service_scgps3b",
+            "template_n6h217g",
             e.target,
-            import.meta.env.VITE_EMAILJS_PUBLIC
+            "Wbnr3P0AMYs1ioFJq"
         )
-
         .then(() => {
             alert("Mensaje enviado correctamente ✅");
             e.target.reset();
-            textareaRef.current.style.height = "auto";
         })
-        .catch(() => {
-            alert("Error al enviar el mensaje ❌");
+        .catch((error) => {
+            console.error("ERROR:", error);
+            alert("Error al enviar ❌");
         });
     };
-
-    useEffect(() => {
-        const textarea = textareaRef.current;
-
-        if (!textarea) return;
-
-        const autoResize = () => {
-            textarea.style.height = "auto";
-            textarea.style.height = textarea.scrollHeight + "px";
-        };
-
-        textarea.addEventListener("input", autoResize);
-
-        return () => {
-            textarea.removeEventListener("input", autoResize);
-        };
-    }, []);
 
     return (
         <div className="contenido-contacto" id="contacto">
             <h2>Contacto</h2>
 
             <div className="contenido-dividido">
-
                 <div className="contenido-left">
                     <form onSubmit={enviarCorreo}>
                         <h3>CONTÁCTAME</h3>
@@ -67,10 +45,9 @@ export default function Contacto() {
                         <div className="llenar">
                             <p>Mensaje</p>
                             <textarea
-                                ref={textareaRef}
                                 name="message"
                                 className="mensaje-textarea"
-                                rows="3"
+                                rows="4"
                                 required
                             ></textarea>
                         </div>
@@ -82,7 +59,6 @@ export default function Contacto() {
                 <div className="contenido-right">
                     <img src={dibujo} alt="dibujo web" />
                 </div>
-
             </div>
         </div>
     );
